0fe3d71448757056d5fb27f523a6213483a58b20
Issufi Badji <issufibadji@gmail.com>
2020-05-17 20:32:37 -0300

    projeto cadastroCliente com laravel

---
+++ app/Jobs/SendEmail.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Support\Facades\Mail;
+use App\Mail\EmailCliente;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class SendEmail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Mail::to('issufibadji@gmail.com')
+                ->send(new EmailCliente($this->user));
+    }
+}
